34507edfdeeade24fbef48fdb49507ce6f162316
oleh1 <lyboleg@gmail.com>
2017-01-21 16:47:32 +0200

    1

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * css and js
+ */
+require get_stylesheet_directory() . '/includes/functions-css-and-js.php';
+
+?>
---
+++ includes/functions-css-and-js.php
@@ -0,0 +1,16 @@
+<?php
+
+function add_theme_scripts() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array( 'jquery' ) );
+
+    wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/bootstrap-3.3.7-dist/css/bootstrap.min.css' );
+    wp_deregister_script( 'jquery' );
+    wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js');
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/bootstrap-3.3.7-dist/js/bootstrap.min.js' );
+}
+add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+?>
