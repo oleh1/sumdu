6b82479c0654180166614c6a1919ae6dca021303
oleh1 <lyboleg@gmail.com>
2017-01-21 17:09:49 +0200

    2

--- includes/functions-css-and-js.php
+++ includes/functions-css-and-js.php
@@ -7,7 +7,7 @@ function add_theme_scripts() {
 
     wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/bootstrap-3.3.7-dist/css/bootstrap.min.css' );
     wp_deregister_script( 'jquery' );
-    wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js');
+    wp_register_script( 'jquery', get_template_directory_uri() . '/jquery-3.1.1.min/jquery-3.1.1.min.js' );
     wp_enqueue_script( 'jquery' );
     wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/bootstrap-3.3.7-dist/js/bootstrap.min.js' );
 }
