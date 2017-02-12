c8ee587452e15e532d5bb48d8334a773ac0fa7f8
Oleh Kopytsia <lyboleg@gmail.com>
2017-02-12 14:30:41 +0200

    7

--- header.php
+++ header.php
@@ -6,23 +6,25 @@
     <?php wp_head(); ?>
 </head>
 <body>
-<header>
-    <div>
-        <?php if(has_custom_logo()) the_custom_logo(); ?>
-    </div>
-    <div>
-        <?php bloginfo(); ?>
-    </div>
-    <div>
-        <?php dynamic_sidebar( 'sidebar-1' ); ?>
-    </div>
-    <div>
+<header id="header">
+        <div class="logo">
+            <?php if(has_custom_logo()) the_custom_logo(); ?>
+        </div>
+        <div class="name_site">
+            <?php bloginfo(); ?>
+        </div>
         <?php
             wp_nav_menu(array(
                 'theme_location' => 'header_menu',
                 'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                'menu_class' => 'header-menu',
+                'menu_class' => 'header-menu'
             ));
         ?>
-    </div>
+        <div class="lang">
+            <ul>
+                <li><a href="http://sumdu/ua/" hreflang="ua" title="Українська (ua)"><img src="<?php echo get_template_directory_uri() ?>/images/lang/ua.png" alt="Українська (ua)" /></a></li>
+                <li><a href="http://sumdu/ru/" hreflang="ru" title="Русский (ru)"><img src="<?php echo get_template_directory_uri() ?>/images/lang/ru.png" alt="Русский (ru)" /></a></li>
+                <li><a href="http://sumdu/en/" hreflang="en" title="English (en)"><img src="<?php echo get_template_directory_uri() ?>/images/lang/en.png" alt="English (en)" /></a></li>
+            </ul>
+        </div>
 </header>
--- includes/functions-css-and-js.php
+++ includes/functions-css-and-js.php
@@ -1,16 +1,21 @@
 <?php
 
-function add_theme_scripts_css() {
-    wp_enqueue_style( 'style', get_stylesheet_uri() );
+function add_theme_scripts_css(){
+    wp_enqueue_style('style', get_stylesheet_uri() );
 
-    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array( 'jquery' ) );
+    wp_enqueue_script('script', get_template_directory_uri().'/js/script.js', array('jquery'));
 
-    wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/bootstrap-3.3.7-dist/css/bootstrap.min.css' );
-    wp_deregister_script( 'jquery' );
-    wp_register_script( 'jquery', get_template_directory_uri() . '/jquery-3.1.1.min/jquery-3.1.1.min.js' );
-    wp_enqueue_script( 'jquery' );
-    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/bootstrap-3.3.7-dist/js/bootstrap.min.js' );
+    wp_enqueue_style('bootstrap-style', get_template_directory_uri().'/bootstrap-3.3.7-dist/css/bootstrap.min.css');
+    wp_deregister_script('jquery' );
+    wp_register_script('jquery', get_template_directory_uri().'/jquery-3.1.1.min/jquery-3.1.1.min.js');
+    wp_enqueue_script('jquery' );
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri().'/bootstrap-3.3.7-dist/js/bootstrap.min.js');
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts_css' );
+
+function add_styles_admin(){
+    wp_enqueue_style('style-admin', get_template_directory_uri().'/css/style-admin.css');
+}
+add_action('admin_head', 'add_styles_admin');
 
 ?>
