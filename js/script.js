296964f5803e514e3e9eeadbd482e068fec9d9cb
Oleh Kopytsia <lyboleg@gmail.com>
2017-02-20 00:22:12 +0200

    11

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
   $("#header .lang li").mouseover(function(){
     $("#header .lang li").css("display", "block");
     $("#header div.lang li.active_lang").css("display", "none");
-
   });
   $("#header .lang ul").mouseout(function(){
     $("#header .lang li").css("display", "none");
