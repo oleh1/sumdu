b4bdfa12e17f0d8d7de2e5765343cf75eb4dc036
Oleh Kopytsia <lyboleg@gmail.com>
2017-02-25 01:21:26 +0200

    13

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
 get_header();
+get_sidebar();
 ?>
-  <div class="content">
+  <div id="content">
     <?php
     if (have_posts()): while (have_posts()): the_post();
-      the_title();
+?>
+      <div align="center" class="title_index"><a href="<?php the_permalink(); ?>"><h1><?php the_title(); ?></h1></a></div>
+<?php
       the_content();
+      the_author();
+      the_date('Y-m-d', '<div class="date_index">', '</div>');
     endwhile;
     endif;
     ?>
--- page.php
+++ page.php
@@ -1,7 +1,8 @@
 <?php
 get_header();
+get_sidebar();
 ?>
-  <div class="content">
+  <div id="content">
     <?php
     if (have_posts()): while (have_posts()): the_post();
       the_title();
