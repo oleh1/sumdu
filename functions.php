1c0cde9e4a63eb5d2b251df437044151a5f1b880
Oleh Kopytsia <lyboleg@gmail.com>
2017-03-10 00:47:40 +0200

    14

--- functions.php
+++ functions.php
@@ -5,21 +5,17 @@ require get_stylesheet_directory() . '/includes/functions-css-and-js.php';
 function sumdu_setup(){
   add_theme_support('custom-logo');
   register_nav_menu('header_menu', 'Header menu');
+  load_theme_textdomain( 'sumdu', get_template_directory().'/languages' );
 }
 add_action('after_setup_theme', 'sumdu_setup');
 
-function theme_slug_widgets_init() {
+function sumdu_sidebar() {
   register_sidebar( array(
-    'name' => __( 'Main Sidebar', 'theme-slug' ),
-    'id' => 'sidebar-1',
-    'description' => __( 'Widgets in this area will be shown on all posts and pages.', 'theme-slug' ),
-    'before_widget' => '<li id="%1$s" class="widget %2$s">',
-    'after_widget'  => '</li>',
-    'before_title'  => '<h2 class="widgettitle">',
-    'after_title'   => '</h2>',
+    'name' => __('Ліва бічна панель', 'sumdu'),
+    'id' => 'left_sidebar',
   ) );
 }
-add_action( 'widgets_init', 'theme_slug_widgets_init' );
+add_action('widgets_init', 'sumdu_sidebar');
 
 function show_lang($lang){
   $lang_url = $_SERVER['REQUEST_URI'];
@@ -40,13 +36,10 @@ function show_lang($lang){
 
 
 
-
 global $wpdb2;
-
 $wpdb2 = new wpdb( 'root', '1', 'DEK', 'localhost' );
 if( ! empty($wpdb2->error) ) wp_die( $wpdb2->error );
 $results = $wpdb2->get_results("SELECT * FROM student");
-
 //var_dump($results);
 
 
--- index.php
+++ index.php
@@ -7,10 +7,14 @@ get_sidebar();
     if (have_posts()): while (have_posts()): the_post();
 ?>
       <div align="center" class="title_index"><a href="<?php the_permalink(); ?>"><h1><?php the_title(); ?></h1></a></div>
-<?php
-      the_content();
-      the_author();
-      the_date('Y-m-d', '<div class="date_index">', '</div>');
+
+      <div class="cont_content">
+        <?php the_content(); ?>
+      </div>
+
+    <div><?php _e('Автор', 'sumdu') ?>: <?php the_author(); ?></div>
+    <div><?php _e('Дата публікації', 'sumdu') ?>: <?php the_date('d.m.Y'); ?> <?php the_time(); ?></div>
+    <?php
     endwhile;
     endif;
     ?>
