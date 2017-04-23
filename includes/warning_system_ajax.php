3bb8b0fbeeaa96e25c80f47e1321b6341b660ede
oleh1 <lyboleg@gmail.com>
2017-04-23 18:35:41 +0300

    mails17

--- includes/warning_system_ajax.php
+++ includes/warning_system_ajax.php
@@ -110,9 +110,7 @@ function f_add_mail()
 
   global $wpdb;
   $wpdb->insert('sumdu_warning_system_mail', array("id" => '', "sumdu_mail" => $text, "id_group" => $id_group), array("%d", "%s", "%d"));
-
-global $wpdb;
-$group = $wpdb->get_results("SELECT * FROM sumdu_warning_system_group ORDER BY id DESC");
+  $group = $wpdb->get_results("SELECT * FROM sumdu_warning_system_group ORDER BY id DESC");
 
 if ($group == null) {
   $i = 1;
@@ -191,6 +189,69 @@ function f_delete_mail()
   $sql = $wpdb->prepare("DELETE FROM sumdu_warning_system_mail WHERE id = %d", $id);
   $wpdb->query($sql);
 
+  $group = $wpdb->get_results("SELECT * FROM sumdu_warning_system_group ORDER BY id DESC");
+
+  if ($group == null) {
+    $i = 1;
+  } else {
+    $i = (int)$group[0]->id_group + 1;
+  }
+  $load = "<div class='overlay-loader'><div class='loader'><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>";
+  ?>
+
+  <div class="content_warning_group">
+    <div class="all_groups">
+      <div class="text_group"><input data-id="<?php echo $i; ?>" class="add_group_i" type="text"></div>
+      <div class="add_group">Додати групу</div>
+      <div class="lo"><div class="load"><?php echo $load; ?></div></div>
+    </div>
+    <div style="clear: both"></div>
+
+    <div class="warning_grop_all">
+      <?php
+      foreach ($group as $result_g) {
+        $mail = $wpdb->get_results("SELECT * FROM sumdu_warning_system_mail WHERE id_group = $result_g->id_group ORDER BY id DESC");
+        ?>
+        <div class="warning_grop" data-id="<?php echo $result_g->id_group; ?>">
+          <div class="sure">
+            <div class="t">Ви точно впевнені?</div>
+            <div class="y_n">
+              <div class="y">Так</div><div class="n">Ні</div>
+            </div>
+          </div>
+          <div class="delete_group">Удалить групу</div>
+          <div class="name_group">
+            <?php echo $result_g->sumdu_group; ?>
+          </div>
+          <?php
+          $all_mails = array();
+          $i = 0;
+          foreach ($mail as $result_m) {
+            $all_mails[$i] = $result_m->sumdu_mail;
+            $i++;
+            ?>
+            <div class="mails">
+              <div class="name_mail"><?php echo $result_m->sumdu_mail; ?></div><div data-id="<?php echo $result_m->id; ?>" class="delete_mail"><img src="<?php echo get_template_directory_uri(); ?>/images/warning_system/delete-mail.png"></div><br>
+            </div>
+            <?php
+          }
+          $all_mails = implode(",", $all_mails);
+          ?>
+          <input data-id_group="<?php echo $result_g->id_group; ?>" class="add_mail_i" type="text">
+          <div class="a_d"><div class="add_mail">Додати email</div></div>
+          <div><input type="text" class="subject_send" placeholder="Тема" value="<?php echo get_option('subject_send-'.$result_g->id_group); ?>"></div>
+          <div><input type="text" class="name_send" placeholder="Ім'я" value="<?php echo get_option('name_send-'.$result_g->id_group); ?>"></div>
+          <div><input type="text" class="mail_send" placeholder="E-mail" value="<?php echo get_option('mail_send-'.$result_g->id_group); ?>"></div>
+          <div class="a_d"><textarea class="message" placeholder="Повідомлення" data-mails="<?php echo $all_mails ?>"><?php echo get_option('message_send-'.$result_g->id_group); ?></textarea></div>
+          <div><div class="send_message">Відправити повідомлення</div></div>
+          <div class="load_message"><?php echo $load; ?></div>
+        </div>
+        <?php
+      }
+      ?>
+    </div>
+  </div>
+<?php
   wp_die();
 }
 ?>
