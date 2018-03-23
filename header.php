ff1dc2fa0a4c3557df25d41516ee9eeca8270884
oleh1 <lyboleg@gmail.com>
2018-03-23 21:13:04 +0200

    analytics

--- header.php
+++ header.php
@@ -4,6 +4,17 @@
     <meta charset="UTF-8">
     <title></title>
     <?php wp_head(); ?>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-116331774-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-116331774-1');
+    </script>
+    
 </head>
 <body>
 <header id="header">
